45a56799ad1e34e0e34425d1cb8fe690020eb68d
nyorofumi <77321593+nyorofumi@users.noreply.github.com>
2021-02-24 13:51:48 +0900

    pushAPISample

---
+++ send.php
@@ -0,0 +1,31 @@
+<?php
+
+$json = '{
+"notification":
+{
+    "title": "テスト送信",
+    "body": "This Is 送信テストです"
+},
+"to": "eYGTl5shvUY:APA91bFPhrud051Mr1bxAGBY9JMJTKsL76jy3s8HcDKGhNks5lFIV93dtycm7mgTnPyCQLgjtUbeokdP2wJ3GsOjEjO4GQ68rwx-HREhxoqUGdSlI9MWJ3gDMSKUVxScrym9xif1qckF"}';
+
+$ch = curl_init();
+
+$headers = array(
+    'Content-Type: application/json',
+    'Authorization: key=AAAAFI4qAe0:APA91bG8coZbR1L6eSF2BL5u41eQRQZWwpMUZJg15I8lHKcpxYDkjFL-RIxE_wzXrCAN0RAwsphRrS1PwG7CyvyF0rCVEMOMH_qsP7z4JMij1Yhuzr8BIBiGQGUHuUzO3bTNpGbRKKg_'
+);
+
+curl_setopt_array($ch, array(
+    CURLOPT_URL => 'https://fcm.googleapis.com/fcm/send',
+    CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POSTFIELDS => $json
+));
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+?>
