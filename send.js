92dbed6ecab7a9b8e295ef52e812e51654bd86db
nyorofumi <77321593+nyorofumi@users.noreply.github.com>
2021-02-26 10:56:18 +0900

    a

--- send.js
+++ send.js
@@ -4,7 +4,7 @@
 
 function sendNotification(){
 	var key = 'AAAAFI4qAe0:APA91bG8coZbR1L6eSF2BL5u41eQRQZWwpMUZJg15I8lHKcpxYDkjFL-RIxE_wzXrCAN0RAwsphRrS1PwG7CyvyF0rCVEMOMH_qsP7z4JMij1Yhuzr8BIBiGQGUHuUzO3bTNpGbRKKg_';
-	var to = 'd2JwRXVCDR4:APA91bFHnRPeJPmtZ5LFMrNTmPRXAKdE-d9myW_fl-ONbPcSlzlT7jOz-GO1w3YXT7l2aV4SiKuA4qIUwq3BTjyZsd10PuVj_QFefwv3isvRW9CQ1HSQoIKU3cqhJtEgR288jhcrD4dV';
+	var to = 'cgs5xST2Gpo:APA91bGwaeVRPf1uFzG1qjqtCdS-f0OeufbIXilogvG7tdsorTjTWCn1rlucUkKJiW_TqE7OfWImRsJ7FLC-ZWn0lSvtACqleEI_iZoMt2BLpg1jHTLwecLX5IgJDX-fBnbORoqzLmrK';
 	var notification = {
 	  'title': 'Portugal vs. Denmark',
 	  'body': '5 to 1',
