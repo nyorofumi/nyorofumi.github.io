6f9c71ad156bd6722c4481c2f61219a9774f0c9a
nyorofumi <77321593+nyorofumi@users.noreply.github.com>
2021-02-26 13:38:34 +0900

    Update send.js

--- send.js
+++ send.js
@@ -4,13 +4,19 @@
 
 function sendNotification(){
 	var key = 'AAAAFI4qAe0:APA91bG8coZbR1L6eSF2BL5u41eQRQZWwpMUZJg15I8lHKcpxYDkjFL-RIxE_wzXrCAN0RAwsphRrS1PwG7CyvyF0rCVEMOMH_qsP7z4JMij1Yhuzr8BIBiGQGUHuUzO3bTNpGbRKKg_';
-	var to = 'cgs5xST2Gpo:APA91bGwaeVRPf1uFzG1qjqtCdS-f0OeufbIXilogvG7tdsorTjTWCn1rlucUkKJiW_TqE7OfWImRsJ7FLC-ZWn0lSvtACqleEI_iZoMt2BLpg1jHTLwecLX5IgJDX-fBnbORoqzLmrK';
+	var to = document.getElementById("id_text").value;
+	//var to = 'd2JwRXVCDR4:APA91bFHnRPeJPmtZ5LFMrNTmPRXAKdE-d9myW_fl-ONbPcSlzlT7jOz-GO1w3YXT7l2aV4SiKuA4qIUwq3BTjyZsd10PuVj_QFefwv3isvRW9CQ1HSQoIKU3cqhJtEgR288jhcrD4dV';
+	//var to = 'cgs5xST2Gpo:APA91bGwaeVRPf1uFzG1qjqtCdS-f0OeufbIXilogvG7tdsorTjTWCn1rlucUkKJiW_TqE7OfWImRsJ7FLC-ZWn0lSvtACqleEI_iZoMt2BLpg1jHTLwecLX5IgJDX-fBnbORoqzLmrK';
+
 	var notification = {
-	  'title': '通知',
-	  'body': 'あなたは??月??日よりアクセスしていません．',
+	  'title': 'Portugal vs. Denmark',
+	  'body': '5 to 1',
 	  'icon': 'firebase-logo.png',
 	  'click_action': 'https://nyorofumi.github.io/'
 	};
+	
+	var target = document.getElementById("nyoro"); 
+	target.innerHTML = to;
 	
 	fetch('https://fcm.googleapis.com/fcm/send', {
 	  'method': 'POST',
@@ -22,10 +28,11 @@ function sendNotification(){
 		'notification': notification,
 		'to': to
 	  })
-	}).then(function(response) {
+	}).then(function(response) 
 	  console.log(response);
 	}).catch(function(error) {
 	  console.error(error);
 	})
+	
 }
 
